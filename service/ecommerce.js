5b37cadad0b3da680dd616a8d96182869e2150a3
Mohamed Ahmed <moh12ahmed2009@gmail.com>
2026-07-24 23:34:08 +0300

    final commit

--- service/ecommerce.js
+++ service/ecommerce.js
@@ -1,11 +1,9 @@
-import { Cart } from '../models/cart.js';
+import Cart from '../models/cart.js';
 
-// Creates a new, empty cart
 const createCartService = async () => {
     return await Cart.create({ items: [] });
 };
 
-// Adds an item to an existing cart
 const addItemToCartService = async (cartId, product, quantity) => {
     return await Cart.findByIdAndUpdate(
         cartId,
@@ -13,8 +11,6 @@ const addItemToCartService = async (cartId, product, quantity) => {
         { runValidators: true, new: true }
     );
 };
-
-// Fetches a cart by ID, with full product details populated
 const getCartService = async (cartId) => {
     return await Cart.findById(cartId).populate('items.product');
 };
